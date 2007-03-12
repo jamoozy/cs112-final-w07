48fd74f83cf0a215f08504e0faf749f284ddd9e8
jamoozy <jamoozy@de016830-462b-0410-93e0-879ebbfe8d4a>
2007-03-12 03:06:25 +0000

    Added some more functions to aide in advance collision.

--- trunk/ cs112-final-w07/include/bouncer.h
+++ trunk/ cs112-final-w07/include/bouncer.h
@@ -8,22 +8,28 @@ class Bouncer
 		void draw();
 		void reset();
 		void start();
+		void stop();
 		bool running();
 		void rotate(double, double);
-		static const float FLOOR = -8;  // Position of the floor (y-coordinate)
-		static const float G = -.001;      // Effect of gravity.
+
+//		static const double RT2 = 1.414;  // The square root of 2
+		static const double FLOOR = 0;   // Position of the floor (y-coordinate)
+		static const double G = -.001;    // Effect of gravity.
 
 	private:
-		float colors[6][4];
-		float vertexes[6][4][3];
+		double colors[6][4];
+		double vertexes[6][4][3];
 
-		float pos[3];      // Position of this Bouncer
-		float velocity[3]; // x, y, z velocities
-		double angle[2];   // The angles showing how much rotation the Bouncer has.
+		double pos[3];      // Position of this Bouncer
+		double velocity[3]; // x, y, z velocities
+		double angle[2];    // The angles showing how much rotation the Bouncer has.
+		double a_velocity[3]; // Angular velocity
+
+		double collision[8]; // Points of collision
 
 		bool gravity_works;
 
-		void collisions();
+		void collide();
 		void physics();
 		void placeVertexes();
 };
